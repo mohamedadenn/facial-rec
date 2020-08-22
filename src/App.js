96e18fe0e295970f1b6bcc4547bac5f7222e1295
mohamed <mohamed.aden@live.ca>
2020-08-22 19:03:04 -0400

    Input form data connects to API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import NavBar from './components/NavBar/NavBar';
 import DetectInput from './components/DetectInput/DetectInput.jsx';
-import Clarifai from 'clarifai';
+import FacialRecognition from './components/FacialRecognition/FacialRecognition.jsx';
+import Clarifai, { COLOR_MODEL } from 'clarifai';
 
 const app = new Clarifai.App({
   apiKey: '3350039e51c64bf68ce0bae5a88d911b'
@@ -11,21 +12,28 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
       }
   }
 
   onInputChange = (event) => {
     console.log(event.target.value);
+    this.setState({input: event.target.value})
   }
 
   onSubmit = () => {
-    console.log('clicked');
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    this.setState({imageUrl: this.state.input});
+
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+       this.state.input)
+    .then(
     function(response) {
-     console.log(response)
+     console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
     },
     function(err) {
-      // there was an error
+    
     }
   );
   }
@@ -36,7 +44,11 @@ class App extends Component {
       <NavBar />
       <DetectInput 
         onInputChange={this.onInputChange} 
-        onSubmit={this.onSubmit}/>
+        onSubmit={this.onSubmit}
+      />
+      <FacialRecognition 
+        imageUrl={this.state.imageUrl}
+        />
       </div>
      );
   }
---
+++ src/components/FacialRecognition/FacialRecognition.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function FacialRecognition({imageUrl}) {
+  
+    return (
+      <div>
+<img src={imageUrl} alt="image"/>
+      </div>
+    );
+  }
+  
