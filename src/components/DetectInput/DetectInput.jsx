9bd9fde57b8a726c85eb0f63e148268900a89200
mohamed <mohamed.aden@live.ca>
2020-08-22 18:15:18 -0400

    Added in initial components boilerplates

---
+++ src/components/DetectInput/DetectInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './DetectInput.css';
+
+export default function DetectInput() {
+  
+    return (
+      <div>
+DETECT INPUT
+      </div>
+    );
+  }
+  
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './NavBar.css';
+
+export default function NavBar() {
+  
+    return (
+      <div>
+NAVBAR
+      </div>
+    );
+  }
+  
