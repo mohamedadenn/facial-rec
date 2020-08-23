871aed70e8167709d904b3d795b1f84b33067b09
mohamed <mohamed.aden@live.ca>
2020-08-22 21:08:40 -0400

    Refactored code to identify groups

--- src/App.js
+++ src/App.js
@@ -13,21 +13,23 @@ class App extends Component {
         this.state = {
             input: '',
             imageUrl: '',
-            box: {},
+            box: [],
         }
     }
     calculateFaceLocation = (data) => {
-        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const clarifaiFace = data.outputs[0].data.regions.map(region => region.region_info.bounding_box);
         const image = document.getElementById('inputimage');
         const height = Number(image.height);
         const width = Number(image.width);
         console.log(clarifaiFace, width, height)
-        return {
-            leftCol: clarifaiFace.left_col * width,
-            topRow: clarifaiFace.top_row * height,
-            rightCol: width - (clarifaiFace.right_col * width),
-            bottomRow: height - (clarifaiFace.bottom_row * height)
-        }
+        return clarifaiFace.map(face => {
+            return {
+                leftCol: face.left_col * width,
+                topRow: face.top_row * height,
+                rightCol: width - face.right_col * width,
+                bottomRow: height - face.bottom_row * height
+            }
+        });
     }
 
     displayFaceBox = (box) => {
@@ -53,18 +55,18 @@ class App extends Component {
     }
 
     render() {
-        return ( <
-            div >
-            <
-            NavBar / >
-            <
-            DetectInput onInputChange = { this.onInputChange }
+        return ( 
+            <div >
+            <NavBar />
+            <DetectInput 
+            onInputChange = { this.onInputChange }
             onSubmit = { this.onSubmit }
-            /> <
-            FacialRecognition imageUrl = { this.state.imageUrl }
+            /> 
+            <FacialRecognition 
+            imageUrl = { this.state.imageUrl }
             box = { this.state.box }
-            /> < /
-            div >
+            /> 
+            </div >
         );
     }
 }
--- src/components/FacialRecognition/FacialRecognition.jsx
+++ src/components/FacialRecognition/FacialRecognition.jsx
@@ -7,13 +7,16 @@ export default function FacialRecognition({imageUrl, box}) {
         <div className='center ma'>
         <div className='absolute mt2'>
           <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+    {box.map(box => (
           <div className='bounding-box' 
           style={{
           top: box.topRow, 
           right: box.rightCol, 
           bottom: box.bottomRow, 
           left: box.leftCol}}></div>
+          ))}
         </div>
+
       </div>
     );
   }
