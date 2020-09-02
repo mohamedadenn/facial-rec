17eaeb24288ad73d52a896d9463cf8c9b54ff51e
mohamed <mohamed.aden@live.ca>
2020-09-01 23:56:56 -0400

    Added Tilt Package for displaying logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import './NavBar.css';
+import Tilt from 'react-tilt';
 
 export default function NavBar() {
   
     return (
-      <nav >
-        
-        Sign Out
-      </nav>
+   
+<div className="ma4 mt0">
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 65 }} style={{ height: 250, width: 250 }} >
+      <img className="Tilt-inner pa3" src={process.env.PUBLIC_URL + '/SpotTheFaces-LOGO.png'}/>
+      </Tilt>
+
+</div>
+
+     
     );
   }
   
