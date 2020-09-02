b54ebbf7f6684a1a6e1e6cf02961d2e7163e55ed
mohamed <mohamed.aden@live.ca>
2020-09-02 01:23:57 -0400

    Added Image Alt Text

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar() {
    
 <div className="pt4">
       <Tilt className="Tilt br2 shadow-2 center" options={{ max : 65 }} style={{ height: 200, width: 200 }} >
-      <img className="Tilt-inner pa3" src={process.env.PUBLIC_URL + '/logo.png'}/>
+      <img alt="Spot The Faces Logo"className="Tilt-inner pa3" src={process.env.PUBLIC_URL + '/logo.png'}/>
       </Tilt>
 
 </div>
